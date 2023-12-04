60b52c2cb62f53db236de88a801034dc3fb34c2e
DESKTOP-1R67ISN\atm_1 <atm192000@gmail.com>
2023-12-04 17:44:49 +0100

    v.25 - Fixed small issues

--- src/sendmail_Torrente/send.php
+++ src/sendmail_Torrente/send.php
@@ -1,68 +1,18 @@
 <?php
-session_start();
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["mensaje"])) {
-    // Obtén el gestor asociado al cliente de la sesión
-    $gestorAsociado = obtenerGestorAsignado($_SESSION['usuari']);
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
 
-<<<<<<< HEAD
-    if ($gestorAsociado) {
-        // Guardar el mensaje en un archivo .txt específico para el gestor
-        $rutaArchivo = "mensajes_gestor/" . $gestorAsociado . ".txt";
-        $contenidoMensaje = "Mensaje de " . $_SESSION['usuari'] . ":\n" . $_POST["mensaje"] . "\nFecha: " . date("Y-m-d H:i:s") . "\n\n";
+session_start(); // Inicia la sesión, asegúrate de llamar a esto al principio del script
 
-        // Escribir el mensaje en el archivo
-        file_put_contents($rutaArchivo, $contenidoMensaje, FILE_APPEND);
-
-        // Redirigir a la página principal o mostrar un mensaje de éxito
-        header("Location: menu.php?mensaje_enviado=true");
-        exit();
-    } else {
-        // Manejar la situación en la que el cliente no tiene un gestor asociado
-        echo "Error: No se encontró un gestor asociado.";
-    }
-}
-
-// Función para obtener el gestor asociado al cliente
-function obtenerGestorAsignado($nombreUsuarioCliente) {
-    // Implementa la lógica para obtener el gestor asociado al cliente
-    // Puedes usar la información proporcionada en la lista de usuarios
-    // Devuelve el nombre de usuario del gestor o null si no se encuentra
-    // Ejemplo de implementación:
-    $usuarios = obtenerListaUsuarios(); // Implementa la función obtenerListaUsuarios()
-    foreach ($usuarios as $usuario) {
-        $detalles = explode(':', $usuario);
-        if ($detalles[1] === $nombreUsuarioCliente) {
-            return $detalles[10]; // Índice 10 corresponde al nombre de usuario del gestor asociado
-        }
-    }
-    return null; // Si no se encuentra el gestor asociado
-}
-
-// Función para obtener la lista de usuarios desde los archivos .txt
-function obtenerListaUsuarios() {
-    $rutaArchivoClientes = "clientes.txt";
-    $rutaArchivoGestores = "gestores.txt";
-
-    // Lee el contenido de los archivos
-    $contenidoClientes = file_get_contents($rutaArchivoClientes);
-    $contenidoGestores = file_get_contents($rutaArchivoGestores);
-
-    // Combina el contenido de ambos archivos
-    $contenidoTotal = $contenidoClientes . "\n" . $contenidoGestores;
-
-    // Convierte el contenido a un array de líneas
-    $lineas = explode("\n", $contenidoTotal);
-
-    // Elimina líneas vacías
-    $lineas = array_filter(array_map('trim', $lineas));
-
-    return $lineas;
-=======
-session_start(); 
-
+// Agrega esta línea para imprimir el contenido de la sesión
+var_dump($_SESSION);
 
 if (isset($_POST["send"])) {
+    // Verifica si el usuario está autenticado
     if (isset($_SESSION['tipus_usuari']) && $_SESSION['tipus_usuari'] === 0) {
 
         $mail = new PHPMailer(true);
@@ -71,24 +21,26 @@ if (isset($_POST["send"])) {
             $mail->isSMTP();
             $mail->Host = 'smtp.gmail.com';
             $mail->SMTPAuth = true;
-            $mail->Username = 'a48465927@gmail.com'; 
-            $mail->Password = 'ghiikpskvgwsgpyt'; 
+            $mail->Username = 'a48465927@gmail.com'; // Reemplaza con tu correo
+            $mail->Password = 'ghiikpskvgwsgpyt'; // Reemplaza con tu contraseña
             $mail->SMTPSecure = 'ssl';
             $mail->Port = 465;
 
-            $mail->setFrom('a48465927@gmail.com'); 
+            $mail->setFrom('tu_correo@gmail.com'); // Reemplaza con tu correo
 
             // Define un array asociativo de usuarios y sus correos electrónicos correspondientes
             $usuariosCorreos = [
                 'client00' => 'a48465927@gmail.com',
                 'client01' => 'atm192000@gmail.com',
+                // Agrega más usuarios y correos electrónicos según sea necesario
             ];
 
+            // Verifica si el usuario actual tiene un correo asociado
             if (isset($usuariosCorreos[$_SESSION['usuari']])) {
                 $correoDestino = $usuariosCorreos[$_SESSION['usuari']];
                 $mail->addAddress($correoDestino);
             } else {
-                throw new Exception("No pots enviar un missatge a aquest usuari");
+                throw new Exception("No puedes enviar un mensaje a este usuario");
             }
 
             $mail->isHTML(true);
@@ -98,46 +50,15 @@ if (isset($_POST["send"])) {
 
             $mail->send();
 
-            echo "<script>alert('Missatge enviat');</script>";
+            echo "<script>alert('Sent Successfully');</script>";
             echo "<script>document.location.href = 'index.php';</script>";
         } catch (Exception $e) {
             echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}<br>";
             echo "Exception: {$e->getMessage()}";
         }
     } else {
-        echo "Acces denegat";
+        // El usuario no está autorizado, redirige o muestra un mensaje de error
+        echo "Unauthorized access";
     }
->>>>>>> origin/atm
 }
 ?>
-
-
-<!DOCTYPE html>
-<html lang="ca">
-
-<head>
-    <meta charset="utf-8">
-    <title>Enviar Mensaje al Gestor - Rellotgeria</title>
-    <link rel="stylesheet" href="./Assets/Stylesheets/agenda.css">
-</head>
-
-<body>
-    <h3><b>Enviar Mensaje al Gestor</b></h3>
-    <form action="enviar_mensaje_gestor.php" method="POST">
-        <label for="mensaje">Mensaje:</label>
-        <textarea name="mensaje" id="mensaje" rows="4" cols="50"></textarea><br><br>
-        <input type="submit" value="Enviar Mensaje">
-    </form>
-
-    <p><a href="menu.php">Volver al menú</a></p>
-
-    <label class="diahora">
-        <?php
-        echo "<p>Usuario utilizando la agenda: " . $_SESSION['usuari'] . "</p>";
-        date_default_timezone_set('Europe/Andorra');
-        echo "<p>Fecha y hora: " . date('d/m/Y h:i:s') . "</p>";
-        ?>
-    </label>
-</body>
-
-</html>
